fb45c118723a8de152a0704023647c9846edf07e
tapukoko <tapukoko9611@gmail.com>
2023-12-19 11:08:28 +0530

    finally after a few errors with chalk@4 connection to mongodb is a success

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const chalk = require("chalk");
+const cors = require('cors');
+const helmet = require('helmet');
 
+
+const keys = require("./config/keys");
+const setupDB = require("./utils/db");
+
+const { port } = keys;
 const app = express();
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Listening");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+        frameguard: true
+    })
+);
+app.use(cors());
+
+setupDB();
+
+app.listen(port, () => {
+    console.log(
+        `${chalk.green('✓')} ${chalk.blue(
+            `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
+        )}`
+    );
 });
