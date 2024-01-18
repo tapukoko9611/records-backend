b9cd9308422c93ef08efe5718200bb243bc7bb2c
tapukoko <tapukoko9611@gmail.com>
2024-01-19 04:08:55 +0530

    last commit

--- routes/api/query.js
+++ routes/api/query.js
@@ -162,6 +162,7 @@ router.get("/employee/:designation&:type", async(req, res) => {
 router.get("/employee", async(req, res) => {
     // get list of all employees with each of them having their total no.of transactions (today, this week, this month, all time)
     try{
+        console.log("Called");
         var employees = await Employee.find();
         // console.log(employees);
         for(var i=0; i<employees.length; i++) {
